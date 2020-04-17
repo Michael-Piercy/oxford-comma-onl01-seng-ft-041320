66366b1d89f7d497764bdcbaed975ec8a86a229f
Michael Piercy <michael.piercy@zoho.com>
2020-04-17 18:02:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
   elsif(array.size == 3)
   return "#{array[0]}, #{array[1]}, and #{array[2]}"
   else (array.size == 5)
-  return "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, #{array[4]} 
+  return "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, #{array[4]} << (")
   end
 end
